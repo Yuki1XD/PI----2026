5a12e9292c2c3c273d656f75f2f0038abffad683
YukiXD <mariasophia1231207@gmial.com>
2026-04-10 08:47:57 -0300

    Changes to be committed: 	new file:   meu-projeto-node/Banco_de_dados/Principal.sql 	new file:   meu-projeto-node/Telas/Cadastro.html 	new file:   meu-projeto-node/index.js 	new file:   meu-projeto-node/node_modules/.package-lock.json

---
+++ meu-projeto-node/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+// Servir arquivos estáticos (HTML, CSS, JS)
+app.use(express.static(path.join(__dirname, "Telas")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "Telas", "Cadastro.html"));
+});
+
+app.listen(3000, () => {
+  console.log("http://localhost:3000");
+});
